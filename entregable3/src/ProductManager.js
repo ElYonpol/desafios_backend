4a30afb635389666320b913765d207ec77d697db
Juan Pablo Portas Esquivel <jppe@yahoo.com.ar>
2023-03-18 18:13:33 -0300

    Corrección

--- entregable3/src/ProductManager.js
+++ entregable3/src/ProductManager.js
@@ -9,7 +9,6 @@ class ProductManager {
 		try {
 			if (fs.existsSync(this.ruta)) {
 				const products = await fs.promises.readFile(this.ruta, "utf-8");
-				console.table(JSON.parse(products));
 				return JSON.parse(products);
 			}
 			throw new Error();
@@ -95,71 +94,4 @@ class ProductManager {
 	};
 }
 
-/* Código para pruebas */
-
-/*const prueba = async () => {
-	let prodManager = new ProductManager("./files/Productos.json");
-
-	await prodManager.getProducts();
-
-	let pruebaProducto1 = {
-		title: "producto prueba",
-		description: "Este es un producto prueba",
-		price: 200,
-		thumbnail: "Sin imagen",
-		code: "abc123",
-		stock: 25,
-	};
-
-	let pruebaProducto2 = {
-		title: "producto prueba 2",
-		description: "Este es un producto prueba 2",
-		price: 300,
-		thumbnail: "Sin imagen",
-		code: "abc124",
-		stock: 2,
-	};
-
-	let pruebaProductoExiste = {
-		title: "producto prueba existe",
-		description: "Este es un producto que existe",
-		price: 400,
-		thumbnail: "Sin imagen",
-		code: "abc123",
-		stock: 5,
-	};
-
-	let pruebaProductoUpdate = {
-		title: "producto prueba",
-		description: "Este es un producto que existe pero mejorado",
-		price: 400,
-		thumbnail: "Sin imagen",
-		code: "abc123",
-		stock: 534,
-	};
-
-	await prodManager.addProduct(pruebaProducto1);
-	await prodManager.getProducts();
-	await prodManager.getProductByID(1);
-
-	await prodManager.addProduct(pruebaProducto2);
-	await prodManager.getProducts();
-	await prodManager.getProductByID(2);
-	await prodManager.getProductByID(3);
-
-	await prodManager.addProduct(pruebaProductoExiste);
-
-	await prodManager.updateProduct({
-		id: 1,
-		title: "producto prueba",
-		description: "Este es un producto que existe pero mejorado",
-		price: 400,
-		thumbnail: "Sin imagen",
-		code: "abc123",
-		stock: 534,
-	});
-
-	await prodManager.deleteProduct(1)
-	await prodManager.deleteProduct(3)
-};
-prueba(); */
+module.exports = ProductManager;
--- entregable3/src/app.js
+++ entregable3/src/app.js
@@ -1,4 +1,5 @@
-const express = require("express")
+const express = require("express");
+const ProductManager = require("./ProductManager.js");
 
 const app = express();
 const PORT = 8080;
@@ -6,27 +7,28 @@ const PORT = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/products", (req,res)=>{
-    res.send("Hola a todos!!!!")
-})
+const products = new ProductManager("./files/Productos.json");
 
-app.get("/products/:pid", (req,res)=>{
-    res.send("Hola a todos!!!!")
-})
-
-app.get("/bienvenida", (peticion, respuesta) => {
-	respuesta.send(
-		"<h1 style='color: blue'>Hola Mundo servidor express ejercicio.</h1>"
-	);
+app.get("/products", async (req, res) => {
+	const productos = await products.getProducts();
+	res.send({ productos });
 });
 
+app.get("/products/:pid", async (req, res) => {
+	const productos = await products.getProducts();
+	const { pid } = req.params;
+	console.log(pid);
 
+	const producto = productos.find((prod) => prod.id === pid);
 
+	if (!producto) return res.status(400).send("No se encontró el producto.");
+
+	res.status(200).send({ producto });
+});
 
 app.listen(PORT, (err) => {
 	if (err) {
 		console.error("Error al iniciar el servidor");
 	}
-
 	console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
